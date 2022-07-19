1c7b64c71e50b90d23a3f26e21ad4fd03c07b3f0
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-07-19 18:05:52 +0530

    files uploaded

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Bx Slider Activation
+$(document).ready(function(){
+    $('.slider_container').bxSlider({
+        auto : 'true',
+        speed : '700',
+        pause : '5000',
+    });
+  });
